19202a93fd3d722832a5b806feb05065b807da1a
Austin Schmidli <austin.schmidli@gmail.com>
2020-11-29 23:10:45 -0700

    major refactor

--- content.js
+++ content.js
@@ -1,21 +1,3 @@
-// Construct the control then inject it into the DOM
-let endcardControl = document.createElement("div");
-endcardControl.innerHTML = `<div
-  id="endcardcontroller"
-  class="ytp-menuitem"
-  role="menuitemcheckbox"
-  aria-checked="true"
-  tabindex="0"
->
-<div class="ytp-menuitem-icon"></div>
-<div class="ytp-menuitem-label">Hide endcards</div>
-<div class="ytp-menuitem-content">
-  <div class="ytp-menuitem-toggle-checkbox"></div>
-</div>
-</div>`;
-endcardControl = endcardControl.firstChild;
-document.querySelector(".ytp-panel-menu").appendChild(endcardControl);
-
 function setEndcardsVisibile(visibility) {
   endcards = document.getElementsByClassName("ytp-ce-element");
   for (i = 0; i < endcards.length; i++) {
@@ -23,32 +5,74 @@ function setEndcardsVisibile(visibility) {
   }
 }
 
-function getControlValue() {
-  // Conditional effectively casts the value from string to boolean
+function getControlState() {
   return (
     document.getElementById("endcardcontroller").getAttribute("aria-checked") ==
     "true"
   );
 }
 
-function setControlValue(value) {
+function setControlState(state) {
   document
     .getElementById("endcardcontroller")
-    .setAttribute("aria-checked", value);
+    .setAttribute("aria-checked", state);
 }
 
-function toggleEndcards() {
-  let checked = getControlValue();
+function handleControl() {
+  // Toggle current state
+  let newState = !getControlState();
 
-  if (checked) setEndcardsVisibile(true);
-  else setEndcardsVisibile(false);
-
-  // Toggle the control
-  setControlValue(!checked);
+  setEndcardsVisibile(newState);
+  setControlState(newState);
+  saveState(newState);
 }
 
-document
-  .getElementById("endcardcontroller")
-  .addEventListener("click", toggleEndcards);
+function injectControl() {
+  const controlHTML = `<div
+    id="endcardcontroller"
+    class="ytp-menuitem" 
+    role="menuitemcheckbox"
+    aria-checked="true"
+    tabindex="0"
+  >
+    <div class="ytp-menuitem-icon"></div>
+    <div class="ytp-menuitem-label">Show endcards</div>
+    <div class="ytp-menuitem-content">
+      <div class="ytp-menuitem-toggle-checkbox"></div>
+    </div>
+  </div>`;
 
-setEndcardsVisibile(false);
+  // Construct the control then inject it into the DOM
+  let endcardControl = document.createElement("div");
+  endcardControl.innerHTML = controlHTML;
+  endcardControl = endcardControl.firstChild;
+  document.querySelector(".ytp-panel-menu").appendChild(endcardControl);
+
+  document
+    .getElementById("endcardcontroller")
+    .addEventListener("click", handleControl);
+}
+
+function saveState(state) {
+  chrome.storage.sync.set({ showEndcards: state });
+}
+
+function loadState() {
+  chrome.storage.sync.get(["showEndcards"], function (result) {
+    state = result.showEndcards;
+
+    setControlState(state);
+    setEndcardsVisibile(state);
+  });
+}
+
+let injectedControl = false;
+document.body.addEventListener("yt-navigate-finish", function (event) {
+  if (!!document.querySelector(".ytp-panel-menu")) {
+    if (!injectedControl) {
+      injectControl();
+      injectedControl = true;
+    }
+    loadState();
+  }
+});
